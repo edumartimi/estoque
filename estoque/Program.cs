c3626fee3b6227e1d087d40387c561f047028a70
edumartimi <eduardomartimiano@hotmail.com>
2021-09-15 15:18:17 -0300

    Adicionar arquivos de projeto.

---
+++ estoque/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+namespace estoque
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //escrevendo para o usuario entrar com o valor
+            Console.WriteLine("Entre com dados do produto: ");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            Console.Write("Quantidade no estoque: ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            // chamando a classe produto
+            Produto p = new Produto(nome, preco, quantidade);
+
+            
+            Console.WriteLine("Dados do produto: " + p);
+            Console.WriteLine("Digite o número de produtos a ser adicionado ao estoque: ");
+    
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProduto(qte);
+
+            // resultado
+            Console.WriteLine("Dados do atualizados: " + p);
+
+            // resultado
+            Console.WriteLine("Digite o número de produtos a serem removidos do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+
+            // resultado
+            Console.WriteLine("Dados do atualizados: " + p);
+
+            Console.ReadKey();
+        }
+    }
+}
