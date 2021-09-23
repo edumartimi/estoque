881ca86e492439ea58fca104c5efee2e9508ad49
edumartimi <eduardomartimiano@hotmail.com>
2021-09-22 22:55:34 -0300

    isso

---
+++ estoque/Produto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+using System.Diagnostics;
+
+namespace estoque
+{
+    [DebuggerDisplay("{" + nameof(GetDebuggerDisplay) + "(),nq}")]
+    public class Produto : ProdutoBase
+    {
+
+        //construindo as variaveis
+        public Produto(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+    }
+}
